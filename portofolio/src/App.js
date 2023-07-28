2b856c42f2727203acab53b24db5c456b5ac94ff
BenoitDx <benoit.berche@gmail.Com>
2023-07-28 14:11:39 +0200

    typo

--- portofolio/src/App.js
+++ portofolio/src/App.js
@@ -1,8 +1,9 @@
 
 import React from 'react';
+import '@fortawesome/fontawesome-free/css/all.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import './styles/global.css'; // Importer le fichier global.css
 
 function App() {
   return (
