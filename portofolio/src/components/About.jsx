7f0aed0abf0b53fd4f73afb698f2b962233299c7
BenoitDx <benoit.berche@gmail.Com>
2023-09-06 02:12:16 +0200

    33%

--- portofolio/src/components/About.jsx
+++ portofolio/src/components/About.jsx
@@ -10,14 +10,20 @@ const Abouts = () => {
             <h3 className='About-title'>À propos</h3>
             <h2 className='About-mention'>de moi</h2>
           </div>
-          <div className='About-icon'>
-            
+          <div className='About-description'>
+            <p> Mon parcours a été marqué par une transition inspirante. Avant de plonger dans le monde du développement web, j'ai exploré différents domaines, allant de la gestion de boutiques en ligne à l'industrie. Ma curiosité pour l'informatique et les nouvelles technologies m'a guidé vers une reconversion que j'ai poursuivie avec détermination.
+
+J'ai acquis des compétences solides grâce à une formation approfondie chez OpenClassrooms, où j'ai obtenu un diplôme de technicien informatique et développeur web. Cette formation m'a permis d'acquérir une expertise dans la création de sites web dynamiques et attrayants. Mon expérience antérieure dans la gestion d'entreprises et mon bagage industriel apportent une perspective unique à mon travail en développement.
+
+Mon approche du développement web est guidée par ma passion pour l'apprentissage constant et l'envie de repousser les limites. Je suis convaincu que chaque projet est une opportunité d'apprendre, de grandir et de créer des solutions qui apportent de la valeur. Mon objectif est de traduire les idées en expériences interactives, élégantes et fonctionnelles.
+
+Je suis enthousiaste à l'idée de collaborer sur des projets innovants et de contribuer à des solutions qui font la différence. Si vous êtes à la recherche d'un développeur passionné par les défis techniques et créatifs, je serais ravi de discuter de la manière dont je peux vous accompagner. Parlons de votre vision et explorons les possibilités ensemble !</p>
 
 
           </div>
         </div>
         <div className='About-scroll'>
-        <i className="fa-solid fa-arrow-down animated-icon"></i>
+        
           </div>  
         </div>
       );
